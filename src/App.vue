f97e0942959b9d8f6081f36a3e293f72f62a865d
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-16 23:19:32 +0200

    Added basic structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import {  } from "module";
+  export default {
+    
+  }
+</script>
+
+<template>
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+  @use './assets/style/general';
+</style>
