5b22c3cdb52586467363419dda961b12f4872e11
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-17 01:00:49 +0200

    Added reset rules for scss Added basic layout for header/nav bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,11 +5,30 @@
 </script>
 
 <template>
-    <div>
-
-    </div>
+    <header>
+        <nav class="container">
+            <div class="row">
+                <ul class="nav__list">
+                    <li>robe</li>
+                    <li>robe</li>
+                    <li>robe</li>
+                    <li>robe</li>
+                </ul>
+                <h2>Boolflix</h2>
+                <div>
+                    <input type="search">
+                    <input type="submit">
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+    .nav__list{
+        display: flex;
+        align-items: center;
+        gap: 6px;
+    }
 
 </style>
