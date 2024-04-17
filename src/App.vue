755027c238141bf9f6c593cb11ec6ecf09861490
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-17 16:39:37 +0200

    Edited main section where cards will be displayed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script>
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
-  import {store} from './store.js';
-  import axios from 'axios';
   export default {
     components:{
       AppHeader,
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,15 +5,30 @@
     export default{
         components:{
             Card
+        },
+        data(){
+            return{
+                store: store
+            }
         }
         
     }
 </script>
 
 <template>
-    <div>
-        <Card/>
-    </div>
+    <main>
+        <section>
+            <div class="container">
+                <h2>I nostri film</h2>
+            </div>
+            <div class="container">
+                <ul v-if="store.movies.lenght !== 0">
+                    <AppCard v-for="movie in store.movies" :key="movie.id" :item="movie"/>
+                </ul>
+                <div v-else>Nessun film corrisponde ai criteri di ricerca.</div>
+            </div>
+        </section>
+    </main>
 </template>
 
 <style lang="scss" scoped>
