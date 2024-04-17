8ac10c6b59029473d2fa213de2d20ab436ff0f5b
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-04-18 00:16:48 +0200

    When title and original title are the same only one will be displayed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -15,7 +15,7 @@
         <div>
             <div><img :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt="Anteprima non disponibile"></div>
             <div>nome{{ item.title }}</div>
-            <div>originale{{ item.original_title }}</div>
+            <div v-if="item.title !== item.original_title">originale{{ item.original_title }}</div>
             <div><img :src="item.original_language" :alt=item.original_language></div>
             <div>voto{{ item.vote_average }}</div>
         </div>
